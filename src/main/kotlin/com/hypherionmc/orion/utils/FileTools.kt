a0c497b58aeca50faff93e48a0bc9287066f1762
HypherionSA <hypherionmc@gmail.com>
2025-06-03 11:39:31 +0200

    [DEV] Fix License Headers

--- src/main/kotlin/com/hypherionmc/orion/utils/FileTools.kt
+++ src/main/kotlin/com/hypherionmc/orion/utils/FileTools.kt
@@ -1,3 +1,9 @@
+/*
+ * This file is part of orion, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2025 HypherionSA and Contributors
+ *
+ */
 package com.hypherionmc.orion.utils
 
 import com.google.gson.Gson
